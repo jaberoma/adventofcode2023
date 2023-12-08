c40e8ded5e6ad524812eec373513b0f89642375f
Javi <jbr.swork@gmail.com>
2023-12-08 11:35:21 +0100

    Day 1 solved.

--- puzzles/day1.py
+++ puzzles/day1.py
@@ -1,27 +1,81 @@
 from shared.common_functions import read_input_file
 
+SPELL_DIGIT_MAP = {
+    'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5,
+    'six': 6, 'seven': 7, 'eight': 8, 'nine': 9
+}
 
-def solve_problem_a():
-    data = read_input_file("../input_data/day1_test1.txt")
+
+def solve_problem_a(input_file):
+    data = read_input_file(input_file)
     total = 0
     for line in data:
-        (first, last) = (None, None)
-        for idx in range(len(line)):
-            if first is None and line[idx].isdigit():
-                first = line[idx]
-
-            last_pos_to_check = len(line)-idx-1
-            if last is None and line[last_pos_to_check].isdigit():
-                last = line[last_pos_to_check]
-
-            if first is not None and last is not None:
-                total += int(first + last)
-                break
+        line_numbers = find_numbers(line)
+        number = line_numbers.get_number()
+        if number is not None:
+            total += number
 
     return total
 
 
-def solve_problem_b():
-    data = read_input_file("../input_data/day1_test2.txt")
-    # Implementation for problem B of Day 1
-    pass
+def solve_problem_b(input_file):
+    data = read_input_file(input_file)
+    total = 0
+    for line in data:
+        line_numbers = find_numbers(line)
+        line_numbers = find_spelled_numbers(line, line_numbers)
+        number = line_numbers.get_number()
+        if number is not None:
+            total += number
+
+    return total
+
+
+def find_numbers(line):
+    line_numbers = LineNumbers(None, None, None, None)
+    for idx in range(len(line)):
+        if line[idx].isdigit():
+            line_numbers.update_first(line[idx], idx)
+
+        last_pos_to_check = len(line) - idx - 1
+        if line[last_pos_to_check].isdigit():
+            line_numbers.update_last(line[last_pos_to_check], last_pos_to_check)
+
+    return line_numbers
+
+
+def find_spelled_numbers(line, line_numbers):
+    texts = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+
+    for text in texts:
+        position = -1
+        while True:
+            position = line.find(text, position + 1)
+            if position == -1:
+                break
+            line_numbers.update_first(SPELL_DIGIT_MAP[text], position)
+            line_numbers.update_last(SPELL_DIGIT_MAP[text], position)
+
+    return line_numbers
+
+
+class LineNumbers:
+    def __init__(self, first, first_idx, last, last_idx):
+        self.first = first
+        self.first_idx = first_idx
+        self.last = last
+        self.last_idx = last_idx
+
+    def update_first(self, new_first, new_first_idx):
+        if self.first is None or new_first_idx < self.first_idx:
+            self.first = new_first
+            self.first_idx = new_first_idx
+
+    def update_last(self, new_last, new_last_idx):
+        if self.last is None or new_last_idx > self.last_idx:
+            self.last = new_last
+            self.last_idx = new_last_idx
+
+    def get_number(self):
+        if self.first is not None and self.last is not None:
+            return int(str(self.first) + str(self.last))
--- tests/test_day1.py
+++ tests/test_day1.py
@@ -1,16 +1,28 @@
 import unittest
+
+from parameterized import parameterized
+
 from puzzles.day1 import solve_problem_a, solve_problem_b
 
 
 class TestDay1(unittest.TestCase):
-    def test_solve_problem_a(self):
-        result = solve_problem_a()
-        expected_result = 54644
+    @parameterized.expand([
+        ("../input_data/day1_test1_sample.txt", 142),
+        ("../input_data/day1_test1.txt", 54644),
+        # Add more test cases as needed
+    ])
+    def test_solve_problem_a(self, input_file, expected_result):
+        result = solve_problem_a(input_file)
         self.assertEqual(expected_result, result)
 
-    def test_solve_problem_b(self):
-        result = solve_problem_b()
-        # Add assertions to test the result
+    @parameterized.expand([
+        ("../input_data/day1_test2_sample.txt", 281),
+        ("../input_data/day1_test2.txt", 53348),
+        # Add more test cases as needed
+    ])
+    def test_solve_problem_b(self, input_file, expected_result):
+        result = solve_problem_b(input_file)
+        self.assertEqual(expected_result, result)
 
 
 if __name__ == '__main__':
